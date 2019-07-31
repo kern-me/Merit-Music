72a1933a949ac7dc7f4fd475c7fb560da31aff0b
Nico Killips <nicokillips@gmail.com>
2019-07-30 23:46:14 -0400

    Update footer.php
    Added new jquery cdn to resolve front-end issues.

--- wp-content/themes/merit-theme/templates/footer.php
+++ wp-content/themes/merit-theme/templates/footer.php
@@ -1,3 +1,7 @@
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"
+  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+  crossorigin="anonymous"></script>
+
 <footer class="content-info">
 <? if(have_rows("columns","option")){ ?>
 	<!--div class="nav">
